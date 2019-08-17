13450948bcf1a0c0f9a2199ef2be30aad63d931f
cjmcauley <christopher.mcauley@gmail.com>
2019-08-17 12:19:43 +0100

    router path created

---
+++ client/src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from '@/App.vue';
+import Projects from '@/views/Projects.vue';
+import Slider from '@/views/Slider.vue';
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: App
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Projects
+    },
+    {
+      path: '/projects/:id',
+      name: 'instructions',
+      component: Slider
+    }
+  ]
+})
+
+export default router;
