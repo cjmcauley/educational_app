87cd7b0d45c77e50027af246d019fa7fee79aed4
cjmcauley <christopher.mcauley@gmail.com>
2019-08-17 16:42:52 +0100

    clickable content

--- client/src/components/ProjectCard.vue
+++ client/src/components/ProjectCard.vue
@@ -1,15 +1,13 @@
 <template lang="html">
   <div>
+
     <div v-if="$route.params.filter === 'indoor'">
-      <a href="#">
-      <div v-if="this.project.indoor === true" class="project-card" >
+      <div v-if="this.project.indoor === true" class="project-card" @click="instructions">
         <h3>{{ project.commonName}}</h3>
         <p>Season: {{ project.growingSeason }}</p>
         <p>Grow Time: {{ project.growTime }}</p>
       </div>
-    </a>
-      <!-- <div v-else>
-      </div> -->
+
     </div>
     <div v-if="$route.params.filter === 'outdoor'">
       <a href="#">
@@ -19,8 +17,7 @@
         <p>Grow Time: {{ project.growTime }}</p>
       </div>
     </a>
-      <!-- <div v-else>
-      </div> -->
+
     </div>
     <div v-if="$route.params.filter === 'edible'">
       <a href="#">
@@ -30,8 +27,7 @@
         <p>Grow Time: {{ project.growTime }}</p>
       </div>
     </a>
-      <!-- <div v-else>
-      </div> -->
+
     </div>
     <div v-if="$route.params.filter === 'not-edible'">
       <a href="#">
@@ -41,8 +37,7 @@
         <p>Grow Time: {{ project.growTime }}</p>
       </div>
     </a>
-      <!-- <div v-else>
-      </div> -->
+
     </div>
   </div>
 
@@ -55,7 +50,19 @@ import ProjectService from '../services/ProjectService.js'
 
 export default {
   name: 'project-card',
-  props: ['project']
+  data () {
+    return {
+      info: []
+    }
+  },
+  props: ['project'],
+  methods: {
+    instructions: function (event) {
+       event => this.project = info;
+      // return console.log('It works');
+
+    }
+  }
 }
 </script>
 
--- client/src/views/Slider.vue
+++ client/src/views/Slider.vue
@@ -1,9 +1,14 @@
 <template lang="html">
+  <div>
 
+  <h1>Instructions</h1>
+
+</div>
 </template>
 
 <script>
 export default {
+  name: 'instructions'
 }
 </script>
 
