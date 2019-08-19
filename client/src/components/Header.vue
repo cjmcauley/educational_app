baaeac138b135b73b5ee7c3fa99fe60a11537f68
cjmcauley <christopher.mcauley@gmail.com>
2019-08-19 16:48:26 +0100

    images added

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,9 +1,11 @@
 <template lang="html">
 <header>
+  <div class="wrapper">
 
-  <router-link :to="{ name: 'home'}"><h1>Home</h1></router-link>
+  <router-link :to="{ name: 'home'}"><v-img class="logo" src="@/assets/elements/logo.png"></v-img></router-link>
   <router-link :to="{ name: 'projects', params: { filter: 'favourite' } }">Favourites</router-link>
   <router-link :to="{ name: 'projects', params: {filter: 'progress'} }">In progress</router-link>
+</div>
 </header>
 
 
@@ -16,11 +18,22 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
+.wrapper{
+  display: flex;
+}
 header{
   background-color: #E8B662;
   padding: 20px 5px;
   color: white;
+}
+.logo{
+  width: 25%;
+  height: auto;
+}
+a{
+  text-decoration: underline #7FCBB0;
+  color: white;
+  font-size: 46px;
 }
 
 </style>
